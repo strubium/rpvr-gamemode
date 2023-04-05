44b8c704daf2f7d58a31846e728ae20a1416bee5
strubium <113206902+strubium@users.noreply.github.com>
2023-04-05 18:29:19 -0500

    Update npc.lua

--- rpvr/gamemode/npc.lua
+++ rpvr/gamemode/npc.lua
@@ -1,67 +1,14 @@
--- Define the class for the zombie nextbot
-DEFINE_BASECLASS("player_zombie")
-
--- Define the hearing range for the zombie
-local HEARING_RANGE = 1000
-
--- Define a function to handle the sound heard event
-local function OnSoundHeard(ent, soundName, volume, origin)
-    -- Check if the entity making the sound is a player
-    if IsValid(ent) and ent:IsPlayer() then
-        -- Find all zombies within the hearing range
-        local zombies = ents.FindInSphere(origin, HEARING_RANGE)
-        
-        -- Alert each zombie within range
-        for _, zombie in ipairs(zombies) do
-            if IsValid(zombie) and zombie:IsNPC() and zombie:GetClass() == "npc_zombie" then
-                zombie:SetEnemy(ent)
-                zombie:UpdateEnemyMemory(ent, origin)
-            end
-        end
-    end
+function SpawnZombies()
+  local spawnPoints = ents.FindByClass("info_player_start") -- Find all spawn points in the map
+  for i=1, 5 do -- Spawn 5 zombies
+    local spawnIndex = math.random(1, #spawnPoints) -- Get a random spawn point index
+    local zombie = ents.Create("npc_fastzombie") -- Create a fast zombie entity
+    zombie:SetPos(spawnPoints[spawnIndex]:GetPos()) -- Set zombie position to the spawn point
+    zombie:SetKeyValue("spawnflags", "512") -- Set the zombie's spawn flags to enable it to see farther
+    zombie:SetKeyValue("MaxRange1", "500") -- Set the maximum range of the zombie's vision to 500 units
+    zombie:SetKeyValue("FieldOfView", "120") -- Set the zombie's field of view to 120 degrees
+    zombie:Spawn() -- Spawn the zombie
+  end
 end
 
--- Add a hook to the "EntityEmitSound" event to detect player sounds
-hook.Add("EntityEmitSound", "OnSoundHeard", OnSoundHeard)
-
--- Define the SpawnZombie function to spawn a zombie at a specified location
-local function SpawnZombie(pos)
-    -- Spawn the NPC
-    local npc = ents.Create("npc_zombie")
-    npc:SetPos(pos)
-    npc:Spawn()
-end
-
--- Define the Initialize function to set the zombie's model and spawn more zombies over time
-function ENT:Initialize()
-    -- Call the base class's Initialize function
-    BaseClass.Initialize(self)
-    
-    -- Set the model of the zombie to headcrab zombie
-    self:SetModel("models/Zombie/Classic.mdl")
-    
-    -- Hook into the sound detection system to make the zombies attack any player they hear
-    hook.Add("EntityEmitSound", "OnSoundHeard", OnSoundHeard)
-
-    -- Spawn zombies at the specified locations
-    local spawnPositions = {
-        Vector(-1000, -1000, 0),
-        Vector(1000, 1000, 0),
-        Vector(-500, 500, 0),
-        Vector(-250, 250, 0),
-        Vector(500, -500, 0),
-        Vector(250, -250, 0),
-    }
-    for _, pos in ipairs(spawnPositions) do
-        SpawnZombie(pos)
-    end
-    
-    -- Spawn more zombies over time
-    timer.Create("SpawnMoreZombies", 10, 0, function()
-        for _, pos in ipairs(spawnPositions) do
-            SpawnZombie(pos)
-        end
-    end)
-end
-
-
+timer.Create("SpawnZombiesTimer", 10, 0, SpawnZombies) -- Create a timer that spawns zombies every 10 seconds
