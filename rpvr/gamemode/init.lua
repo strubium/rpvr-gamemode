e33b26d0eba0ac71346b7ca43f6bf61d6ab14fd7
strubium <113206902+strubium@users.noreply.github.com>
2023-04-05 13:46:24 -0500

    Update init.lua

--- rpvr/gamemode/init.lua
+++ rpvr/gamemode/init.lua
@@ -9,6 +9,7 @@ include("shared.lua")
 include("npc.lua")
 include("weather.lua")
 
+-- Gets the rate that zombies respawn as inputted in the main menu
 function GetZrate()
     local zrate = GetConVar("rpvr_zrate"):GetInt()
     if zrate == 0 then
@@ -17,3 +18,12 @@ function GetZrate()
         print("[INFORMATION] rpvr_zrate works!")
     end
 end
+-- Define a hook function that runs the "atmos_enabled 1" command when rpvr_dncycle is set to 1
+local function OnRpvrDnCycleChanged(pl, convar, oldValue, newValue)
+    if convar:GetName() == "rpvr_dncycle" and newValue == "1" then
+        RunConsoleCommand("atmos_enabled", "1")
+    end
+end
+
+-- Add the hook to the OnClientLuaChanged event
+hook.Add("OnClientLuaChanged", "OnRpvrDnCycleChanged", OnRpvrDnCycleChanged)
