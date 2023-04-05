b9f376cb33c28e4d6e7ff4f7ba44db1cf9d88b40
strubium <113206902+strubium@users.noreply.github.com>
2023-04-05 14:10:46 -0500

    Update npc.lua
    Added the lua code for spawning zombies

--- rpvr/gamemode/npc.lua
+++ rpvr/gamemode/npc.lua
@@ -1,26 +1,59 @@
+-- Define the class for the zombie nextbot
+DEFINE_BASECLASS("player_zombie")
 
-function SpawnRandomNPC()
-    -- define NPC types and spawn positions
-    local npcTypes = { "npc_citizen", "npc_combine_s" }
+-- Define the hearing range for the zombie
+local HEARING_RANGE = 1000
+
+-- Define a function to handle the sound heard event
+local function OnSoundHeard(ent, soundName, volume, origin)
+    -- Check if the entity making the sound is a player
+    if IsValid(ent) and ent:IsPlayer() then
+        -- Find all zombies within the hearing range
+        local zombies = ents.FindInSphere(origin, HEARING_RANGE)
+        
+        -- Alert each zombie within range
+        for _, zombie in ipairs(zombies) do
+            if IsValid(zombie) and zombie:IsNPC() and zombie:GetClass() == "npc_zombie" then
+                zombie:SetEnemy(ent)
+                zombie:UpdateEnemyMemory(ent, origin)
+            end
+        end
+    end
+end
+
+-- Add a hook to the "EntityEmitSound" event to detect player sounds
+hook.Add("EntityEmitSound", "OnSoundHeard", OnSoundHeard)
+
+-- Define the SpawnZombie function to spawn a zombie at a specified location
+local function SpawnZombie(pos)
+    -- Spawn the NPC
+    local npc = ents.Create("npc_zombie")
+    npc:SetPos(pos)
+    npc:Spawn()
+end
+
+-- Define the Initialize function to set the zombie's model and hook into the sound detection system
+function ENT:Initialize()
+    -- Call the base class's Initialize function
+    BaseClass.Initialize(self)
+    
+    -- Set the model of the zombie to headcrab zombie
+    self:SetModel("models/headcrab.mdl")
+
+    -- Hook into the sound detection system to make the zombies attack any player they hear
+    hook.Add("EntityEmitSound", "OnSoundHeard", OnSoundHeard)
+
+    -- Spawn zombies at the specified locations
     local spawnPositions = {
         Vector(-1000, -1000, 0),
         Vector(1000, 1000, 0),
         Vector(-500, 500, 0),
-		Vector(-250, 250, 0),
+        Vector(-250, 250, 0),
         Vector(500, -500, 0),
-		Vector(250, -250, 0),
+        Vector(250, -250, 0),
     }
-
-    -- pick a random NPC type and spawn position
-    local npcType = table.Random(npcTypes)
-    local spawnPos = table.Random(spawnPositions)
-
-    -- spawn the NPC
-    local npc = ents.Create(npcType)
-    npc:SetPos(spawnPos)
-	npc:SetKeyValue("additionalequipment", "weapon_ar2") -- Add additional equipment to NPC
-    npc:Spawn()
+    for _, pos in ipairs(spawnPositions) do
+        SpawnZombie(pos)
+    end
 end
 
--- spawn NPCs every 5 seconds
-timer.Create("SpawnRandomNPCs", 5, 0, SpawnRandomNPC)
